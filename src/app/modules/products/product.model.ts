94505a396dc279198570d71ce2a233b4d95f1cef
Soumadip <39thdecember@gmail.com>
2025-06-04 23:54:31 +0530

    Product Model ☑️

---
+++ src/app/modules/products/product.model.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import { TProduct, TVariants, TIventory } from "./product.interface";
+
+const VariantSchema = new mongoose.Schema<TVariants>({
+  type: String,
+  value: String,
+});
+
+const InventorySchema = new mongoose.Schema<TIventory>({
+  quantity: Number,
+  inStock: Boolean,
+});
+
+// Product Schema And Model
+const ProductSchema = new mongoose.Schema<TProduct>({
+  name: { type: String, required: true },
+  description: String,
+  price: Number,
+  category: String,
+  tags: [String],
+  variants: [VariantSchema],
+  inventory: [InventorySchema],
+});
+
+const Product =
+  mongoose.models.Product || mongoose.model("Product", ProductSchema);
+
+export default Product;
